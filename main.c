52dde231b3eb209b10f6018f7ca5b2c792a3a318
Arthur CAUCHY <acauchy@e1r5p1.42.fr>
2018-01-24 14:24:20 +0100

    update

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: acauchy <acauchy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/20 12:03:19 by acauchy           #+#    #+#             */
-/*   Updated: 2018/01/20 12:29:14 by acauchy          ###   ########.fr       */
+/*   Updated: 2018/01/24 13:47:22 by acauchy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,70 @@ static void	print_prompt(void)
 	ft_putstr_fd(PROMPT, 1);
 }
 
-static char *read_input(void)
+static void	exit_error(char *errmsg)
+{
+	if (errmsg)
+		ft_putendl_fd(errmsg, 2);
+	else
+		ft_putendl_fd("error", 2);
+	exit(1);
+}
+
+static char *ask_for_input(void)
 {
 	static char	input[4096];
-	size_t		size_read = 0;
+	int			size_read;
 
+	print_prompt();
 	size_read = read(0, input, 4096);
-	/*if (size_read == -1)
-		exit_error("error: read");*/
+	if (size_read == -1)
+		exit_error("read() error");
 	input[size_read] = '\0';
+	if (size_read > 0 && input[size_read - 1] == '\n')
+		input[size_read - 1] = '\0';
 	return (ft_strdup(input));
+}
+
+static void start_process(char *input)
+{
+	pid_t	pid;
+	int		status;
+
+	pid = fork();
+	if (pid == -1)
+		exit_error("fork() error");
+	if (pid == 0)
+	{
+		ft_putendl(input);
+		free(input);
+		exit(0);
+	}
+	else
+	{
+		wait(&status);
+		if (status == -1)
+			exit_error("wait() error");
+	}
 }
 
 int			main(void)
 {
-	print_prompt();
-	ft_putendl(read_input());
+	char	*rep;
+	
+	while ((rep = ask_for_input()))
+	{
+		if (strcmp(rep, "") == 0)
+		{
+			free(rep);
+			continue;
+		}
+		if (strcmp(rep, "exit") == 0)
+		{
+			free(rep);
+			break ;
+		}
+		start_process(rep);
+		free(rep);
+	}
 	return (0);
 }
--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: acauchy <acauchy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/20 12:10:52 by acauchy           #+#    #+#             */
-/*   Updated: 2018/01/20 12:27:27 by acauchy          ###   ########.fr       */
+/*   Updated: 2018/01/24 12:33:47 by acauchy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 # define __MINISHELL_H
 
 # include <sys/types.h>
+# include <sys/wait.h>
 # include <sys/uio.h>
 # include <unistd.h>
+# include <stdlib.h>
 # include "libft.h"
 
-# define PROMPT "$> "
+# define PROMPT "segvsh$ "
 
 #endif
