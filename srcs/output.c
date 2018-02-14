c64ae81f8cccdccbfaed5f4956fb906ec0ec17ca
ArthurCauchy <arthur.cauchy@free.fr>
2018-02-14 17:15:32 +0100

    updated prompt

--- srcs/output.c
+++ srcs/output.c
@@ -6,7 +6,7 @@ void	print_prompt(t_env **env)
 
 	if ((user = read_from_env(env, "USER")))
 		ft_miniprint("\033[1;31;40m%r0s%'s ", user);
-	ft_putstr("\033[1;33;40mcash$ ");
+	ft_putstr("\033[1;31;40mminish$ ");
 	ft_putstr("\033[0m");
 	free(user);
 }
