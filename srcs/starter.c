b685f0748516e054704b67f437caaf32ac3d9177
Arthur CAUCHY <acauchy@e1r5p7.42.fr>
2018-02-22 15:31:04 +0100

    little fix in the command starter

--- srcs/starter.c
+++ srcs/starter.c
@@ -6,7 +6,7 @@
 /*   By: acauchy <acauchy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/20 12:03:19 by acauchy           #+#    #+#             */
-/*   Updated: 2018/02/22 14:19:33 by acauchy          ###   ########.fr       */
+/*   Updated: 2018/02/22 15:19:12 by acauchy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,30 +18,38 @@ static void	replace_exec_name(char **arg, char *newname)
 	*arg = newname;
 }
 
+static void	try_start_process(t_env **cmd_env, char **args)
+{
+	if (is_executable(args[0]))
+		start_process(cmd_env, args);
+	else
+		ft_fminiprint(2, "%l0s%: Permission denied.\n", args[0]);
+}
+
 void		start_command(t_env **env, t_env **cmd_env, char **args)
 {
 	int		retcode;
 	char	*after_path;
+	int		notfound;
 
 	if (!args[0])
 		return ;
+	notfound = 1;
 	if ((retcode = search_start_builtin(cmd_env, args)) == -2)
 	{
-		if (ft_strchr(args[0], '/') && !is_there_a_file(args[0]))
+		if (ft_strchr(args[0], '/'))
 		{
-			ft_fminiprint(2, "%l0s%: Command not found.\n", args[0]);
-			return ;
+			if (is_there_a_file(args[0]))
+				notfound = 0;
 		}
-		if ((after_path = find_cmd_path(env, cmd_env, args[0])))
+		else if ((after_path = find_cmd_path(env, cmd_env, args[0])))
+		{
 			replace_exec_name(&args[0], after_path);
-		else
-		{
-			ft_fminiprint(2, "%l0s%: Command not found.\n", args[0]);
-			return ;
+			notfound = 0;
 		}
-		if (is_executable(args[0]))
-			start_process(cmd_env, args);
+		if (notfound == 1)
+			ft_fminiprint(2, "%l0s%: Command not found.\n", args[0]);
 		else
-			ft_fminiprint(2, "%l0s%: Permission denied.\n", args[0]);
+			try_start_process(cmd_env, args);
 	}
 }
